c317fff7dfe892e987dd0217ffa08e0fb75fde55
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2025-08-31 19:19:08 +0300

    Add input reading macro and command prompt loop
    Introduced a read_string macro in macros.asm for reading user input. Updated main.asm to display a command prompt and read input into a session buffer, refactoring message variables and improving program structure.

--- macros.asm
+++ macros.asm
@@ -1,3 +1,4 @@
+; print
 print_string macro input_string
     mov AH, 09h
     mov DX, offset input_string
@@ -8,4 +9,25 @@ print_char macro input_char
     mov AH, 02h
     mov DL, input_char
     int 21h
+endm
+
+; read
+read_string macro output_string
+    LOCAL read_string_loop, read_string_end
+    mov SI, 0
+read_string_loop:
+    mov AH, 01h
+    int 21h
+    
+    cmp AL, 13 ; enter
+    je read_string_end
+    
+    cmp SI, 11
+    je read_string_end
+    
+    mov output_string[SI], AL
+    
+    inc SI
+    jmp read_string_loop
+read_string_end:
 endm
--- main.asm
+++ main.asm
@@ -4,30 +4,43 @@
     ; program data
     asmora_version db "1.0", '$'
     asmora_author db "Catalin Grigoriev", '$'
+    asmora_uname_command db "asmora > ", '$'
 
-    welcome_msg db "Welcome to Asmora", '$'
-    welcome_msg_created_by db "Created by ", '$'
+    ; meesages
+    msg_welcome db "Welcome to Asmora", '$'
+    msg_welcome_created_by db "Created by ", '$'
+    
+    ; session
+    session_input db 12 DUP(' ')
 .code
 
 include macros.asm
 include procs.asm
 
 MAIN:
+MAIN_INIT:
     mov AX, @data
     mov DS, AX
     
     ; welcome message
-    print_string welcome_msg
+    print_string msg_welcome
     print_char " "
     print_string asmora_version
     
     call print_new_line
     
-    print_string welcome_msg_created_by
+    print_string msg_welcome_created_by
     print_char " "
     print_string asmora_author
     
-
+    call print_new_line
+    call print_new_line
+    
+MAIN_LOOP:
+    print_string asmora_uname_command
+    read_string session_input
+    
+MAIN_END:
     mov AH, 4Ch
     int 21h
 end MAIN
